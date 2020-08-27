135d923e8562542fc2566649d9150a09c24ebda6
Scott Gruber <scott@scottgruber.me>
2020-08-27 10:22:52 -0700

    first commit

--- ucla-admin-color-schemes.php
+++ ucla-admin-color-schemes.php
@@ -54,24 +54,24 @@ function add_colors() {
 		'ucla-light-theme',
 		__( 'UCLA Light Theme', 'admin_schemes' ),
 		get_color_url( 'ucla-light-theme' ),
-		// array( '#2774ae', '#003b5c', '#005587', '#8cb8e8' ),
-		// array(
-		// 	'base' => '#003b5c',
-		// 	'focus' => '#0079bf',
-		// 	'current' => '#ffd100',
-		// )
+		array( '#2774ae', '#003b5c', '#005587', '#ffd100' ),
+		array(
+			'base' => '#003b5c',
+			'focus' => '#0079bf',
+			'current' => '#ffd100',
+		)
 	);
 
 	wp_admin_css_color(
 		'ucla-dark-theme',
 		__( 'UCLA Dark Theme', 'admin_schemes' ),
 		get_color_url( 'ucla-dark-theme' ),
-		// array( '#2774ae', '#003b5c', '#005587', '#8cb8e8' ),
-		// array(
-		// 	'base' => '#f1f3f3',
-		// 	'focus' => '#fff',
-		// 	'current' => '#fff',
-		// )
+		array( '#2774ae', '#003b5c', '#005587', '#ffd100' ),
+		array(
+			'base' => '#f1f3f3',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
 	);
 
 }
